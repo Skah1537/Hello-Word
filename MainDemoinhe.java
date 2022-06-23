51afc285c5b281db77915fbdfdca326f151f4c50
Skah1537 <105586434+Skah1537@users.noreply.github.com>
2022-06-23 20:13:03 +0500

    Add files via upload

---
+++ MainDemoinhe.java
@@ -0,0 +1,48 @@
+class A
+{
+int i;
+int j;
+A(int x, int y )
+{
+i=x;
+j=y;
+}
+void show()
+{
+System.out.println("i="+i +"j="+j);
+}
+int sum()
+{
+return(+);
+}
+}
+class B extends A
+{
+int k;
+B(int x, int y, int z)
+{
+syper(x,y);
+}
+void showk();
+{
+System.out.println("i="+i+i+"j"+"k="+k);
+}
+int showk();
+{
+return i+j+k;
+}
+}
+class MainDemoinheeitance
+{
+public static void main(String[]args)
+{
+A objA=newA(5,10);
+B objB=newB(10,20,30);
+
+int amA=objA.sum();
+int amB=objb.sum();
+obj show();
+obj showk();
+system.out.println("amA"=+amA);
+}
+}
